9ecd46e0ab155876e7e8b85ae982d9d9f92682ae
MrFabkirox <fabrice.esope@gmail.com>
2018-10-18 16:53:38 +0800

    progression on react first page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import AppNavbar from './components/AppNavbar';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +23,10 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
+
+        <AppNavbar />
+
       </div>
     );
   }
